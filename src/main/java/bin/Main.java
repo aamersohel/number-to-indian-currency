4771789f981b047de14ad78d80be6d4d8407682f
Aamer Sohel <aamersohel7@gmail.com>
2018-10-13 20:42:13 +0530

    Project initial setup is done

---
+++ src/main/java/bin/Main.java
@@ -0,0 +1,11 @@
+
+package bin;
+
+public class Main {
+    public static void main(String... args) {
+        
+        
+        
+        System.out.println("This is main 2 class");
+    }
+}
---
+++ src/main/java/com/aamersohel/git/java/IndianCurrency.java
@@ -0,0 +1,7 @@
+package com.aamersohel.git.java;
+
+public class IndianCurrency {
+    public IndianCurrency() {
+        System.out.println("DONE_UI");
+    }
+}
